77c0320493e8ed56da446e1ba83e5214837c73ef
imekenye <ismekenye@gmail.com>
2020-11-23 19:28:12 +0300

    feat: add filterInput component

--- src/components/FilterInput/index.js
+++ src/components/FilterInput/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Container, Checkbox, CheckboxLabel } from './styles/filterinput';
+import {
+  Container,
+  Checkbox,
+  CheckboxLabel,
+  Button,
+} from './styles/filterinput';
 
 export default function FilterInput({ children, ...rest }) {
   return <Container {...rest}>{children}</Container>;
 }
 
-FilterInput.Checkbox = function FilterInputCheckbox({ children, ...rest }) {
-  return <Checkbox {...rest}>{children}</Checkbox>;
+FilterInput.Checkbox = function FilterInputCheckbox({ ...rest }) {
+  return <Checkbox {...rest} />;
 };
 
 FilterInput.CheckboxLabel = function FilterInputCheckboxLabel({
@@ -14,4 +19,8 @@ FilterInput.CheckboxLabel = function FilterInputCheckboxLabel({
   ...rest
 }) {
   return <CheckboxLabel {...rest}>{children}</CheckboxLabel>;
+};
+
+FilterInput.Button = function FilterInputButtonx({ children, ...rest }) {
+  return <Button {...rest}>{children}</Button>;
 };
--- src/components/FilterInput/styles/filterinput.js
+++ src/components/FilterInput/styles/filterinput.js
@@ -1,5 +1,48 @@
 import styled from 'styled-components/macro';
 
-export const Container = styled.main``;
-export const Checkbox = styled.div``;
-export const CheckboxLabel = styled.div``;
+export const Container = styled.div`
+  position: absolute;
+  right: 70px;
+  top: 92px;
+  width: 172px;
+  /* height: 192px; */
+  background: #fff;
+  box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  padding: 34px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  .filter__input {
+    display: flex;
+  }
+`;
+export const Checkbox = styled.input.attrs({ type: 'checkbox' })`
+  border: 0;
+  clip: rect(0 0 0 0);
+  clip-path: inset(50%);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  white-space: nowrap;
+  width: 1px;
+  margin-right: 8px;
+`;
+
+export const CheckboxLabel = styled.label`
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  padding-bottom: 8px;
+`;
+export const Button = styled.button`
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.secondary};
+  border: none;
+  border-radius: 4px;
+  padding: 8px 0;
+  cursor: pointer;
+`;
