ceb2b1cd849f1de10d3c07a5e00573a1c549ebe0
imekenye <ismekenye@gmail.com>
2020-11-23 19:19:40 +0300

    refactor: use useReducer hook

--- src/hooks/useFetchPlaces.js
+++ src/hooks/useFetchPlaces.js
@@ -1,24 +1,70 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer } from 'react';
 import useCurrentLocation from './useCurrentLocation';
 import axios from 'axios';
 
+const ACTIONS = {
+  MAKE_REQUEST: 'make-request',
+  GET_DATA: 'get-data',
+  ERROR: 'error',
+};
+
+const BASE_URL = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=34.102310,-118.340027&radius=1500&type=restaurant&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.MAKE_REQUEST:
+      return { loading: true, restaurants: [] };
+    case ACTIONS.GET_DATA:
+      return {
+        ...state,
+        loading: false,
+        restaurants: action.payload.restaurants,
+      };
+    case ACTIONS.ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+        restaurants: [],
+      };
+    default:
+      return state;
+  }
+}
+
 export default function useFetchPlaces() {
-  const [restaurants, setRestaurants] = useState();
+  const [state, dispatch] = useReducer(reducer, {
+    restaurants: [],
+    loading: true,
+  });
   const { location } = useCurrentLocation();
 
-  const Places_url = location
-    ? `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${
-        location && location.latitude
-      },${location && location.longitude}&radius=1500&type=restaurant&key=${
-        process.env.REACT_APP_GOOGLE_MAPS_API_KEY
-      }`
-    : `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=34.102310,-118.340027&radius=1500&type=restaurant&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
-  const fetchPlaces = async () => {
-    const res = await axios.get(Places_url);
-    setRestaurants(res.data.results);
-  };
+  const BASE_URL2 = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${
+    location && location.latitude
+  },${location && location.longitude}&radius=1500&type=restaurant&key=${
+    process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+  }`;
+
   useEffect(() => {
-    fetchPlaces();
-  }, [Places_url]);
-  return { restaurants };
+    const cancelToken1 = axios.CancelToken.source();
+    dispatch({ type: ACTIONS.MAKE_REQUEST });
+
+    axios
+      .get(BASE_URL2, { cancelToken: cancelToken1.token })
+      .then((res) => {
+        dispatch({
+          type: ACTIONS.GET_DATA,
+          payload: { restaurants: res.data.results },
+        });
+      })
+      .catch((e) => {
+        if (axios.isCancel(e)) return;
+        dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
+      });
+
+    return () => {
+      cancelToken1.cancel();
+    };
+  }, []);
+  return state;
 }
