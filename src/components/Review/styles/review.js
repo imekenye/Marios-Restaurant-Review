33d0fadaac451d5b370d97f3a9ab2d1ced41825f
imekenye <ismekenye@gmail.com>
2020-12-02 17:32:38 +0300

    feat: add review component

--- src/components/Review/styles/review.js
+++ src/components/Review/styles/review.js
@@ -1,9 +1,35 @@
 import styled from 'styled-components/macro';
 
-export const Container = styled.main``;
-export const Header = styled.div``;
-export const Button = styled.button``;
-export const Body = styled.div``;
-export const Title = styled.h2``;
+export const Container = styled.div`
+  width: 100%;
+`;
+export const Header = styled.div`
+  position: fixed;
+  top: 106px;
+  left: 50%;
+  width: 100%;
+  margin-bottom: 36px;
+  background: #fcfcfc;
+  padding: 30px 76px;
+`;
+export const Button = styled.button`
+  width: 96px;
+  height: 36px;
+  border: none;
+  color: #454444;
+  background: #f5d8bf;
+  margin-top: 16px;
+`;
+export const Body = styled.div`
+  padding-top: 150px;
+`;
+export const Title = styled.h2`
+  font-weight: bold;
+  color: #454444;
+  font-size: 18px;
+`;
 export const Rating = styled.div``;
-export const Text = styled.p``;
+export const Text = styled.p`
+  font-size: 14px;
+  color: rgba(69, 68, 68, 0.7);
+`;
---
+++ src/containers/reviewContainer.js
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react';
+import { Review, Restaurant } from '../components';
+import StarRating from '../components/StarRating';
+import PlacesContext from '../contexts/places-context';
+
+export default function ReviewContainer() {
+  const { reviews } = useContext(PlacesContext);
+  return (
+    <>
+      <Review>
+        <Review.Header>
+          <Restaurant.Title>{reviews.name}</Restaurant.Title>
+          <Restaurant.Location>{reviews.formatted_address}</Restaurant.Location>
+          <Restaurant.Rating>
+            <StarRating total={reviews.rating} />
+          </Restaurant.Rating>
+          <Review.Button>Add Review</Review.Button>
+        </Review.Header>
+        {/* {reviews.reviews === 'undefined' && } */}
+        {console.log(reviews.reviews)}
+        <Review.Body>
+          {reviews.reviews &&
+            reviews.reviews.map((review) => (
+              <>
+                <Review.Title>{review.author_name}</Review.Title>
+                <Review.Rating>
+                  <StarRating total={review.rating} />
+                </Review.Rating>
+                <Review.Text>{review.text}</Review.Text>
+              </>
+            ))}
+        </Review.Body>
+      </Review>
+    </>
+  );
+}
